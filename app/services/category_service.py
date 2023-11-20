fb7b1b43385163173bd892b83a803493e86a3448
d3nnyyy <d3nnn41k@gmail.com>
2023-11-20 16:38:51 +0200

    implemented category service

---
+++ app/services/category_service.py
@@ -0,0 +1,17 @@
+from app.dao.category_dao import get_all_categories_dao, create_category_dao, update_category_dao, delete_category_dao
+
+
+def get_all_categories():
+    return get_all_categories_dao()
+
+
+def create_category(data):
+    return create_category_dao(data)
+
+
+def update_category(name, data):
+    return update_category_dao(name, data)
+
+
+def delete_category(name):
+    return delete_category_dao(name)

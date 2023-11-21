0f4e0af89589a31708d676bfdd5c806d8529e460
d3nnyyy <d3nnn41k@gmail.com>
2023-11-21 11:48:15 +0200

    implemented crud for drug service

--- app/services/drug_service.py
+++ app/services/drug_service.py
@@ -1,5 +1,21 @@
-from app.dao.drug_dao import get_all_drugs_dao
+from app.dao.drug_dao import get_all_drugs_dao, get_drug_by_id_dao, create_drug_dao, update_drug_dao, delete_drug_dao
 
 
 def get_all_drugs():
     return get_all_drugs_dao()
+
+
+def get_drug_by_id(id):
+    return get_drug_by_id_dao(id)
+
+
+def create_drug(data):
+    return create_drug_dao(data)
+
+
+def update_drug(id, data):
+    return update_drug_dao(id, data)
+
+
+def delete_drug(id):
+    return delete_drug_dao(id)
